e1cfa9c6dae5a7f3076808cb172d40c96b84dc95
Kayla I Perry <kiperry1488@gmail.com>
2025-03-28 14:26:59 -0400

    updated code

--- 2023prelimNMDS.R
+++ 2023prelimNMDS.R
@@ -1,9 +1,15 @@
 library(readr)
 dat <- read_csv("C:/Users/mgj06/OneDrive/Documents/grad/decomp2023NAfix.csv")
+dat <- read.csv("./decomp2023final.csv")
 
 dat$site <- as.factor(dat$Site)
 dat$trt <- as.factor(dat$Trt)
 dat$trap <- as.factor(dat$Trap)
+
+dat$Site <- as.factor(dat$Site)
+dat$Trt <- as.factor(dat$Trt)
+dat$Trap <- as.factor(dat$Trap)
+str(dat)
 
 library(lme4)
 # install.packages("blmeco")
@@ -37,12 +43,12 @@ stressplot(nmds.decom)
 plot(nmds.decom) # basic plot with no trt distinctions
 
 # plot the NMDS model
-ordiplot(nmds.decom, disp = "sites", type = "n", xlim = c(-1, 1), ylim = c(-1, 1))
-points(nmds.decom, dis = "sites", select = which(dat$trt=="1"), pch = 17, cex = 2, col = "#73D055FF")
-points(nmds.decom, dis = "sites", select = which(dat$trt=="2"), pch = 18, cex = 2, col = "#481567FF")
-points(nmds.decom, dis = "sites", select = which(dat$trt=="3"), pch = 15, cex = 2, col = "#2D708EFF")
-levels(dat$trt)
-ordiellipse(nmds.decom, groups = dat$trt, draw = "lines", col = c("#73D055FF", "#481567FF", "#2D708EFF"), 
+ordiplot(nmds.decom, disp = "sites", type = "n", xlim = c(-3.5, 2), ylim = c(-1.5, 1))
+points(nmds.decom, dis = "sites", select = which(dat$Trt=="1"), pch = 17, cex = 2, col = "#73D055FF")
+points(nmds.decom, dis = "sites", select = which(dat$Trt=="2"), pch = 18, cex = 2, col = "#481567FF")
+points(nmds.decom, dis = "sites", select = which(dat$Trt=="3"), pch = 15, cex = 2, col = "#2D708EFF")
+levels(dat$Trt)
+ordiellipse(nmds.decom, groups = dat$Trt, draw = "lines", col = c("#73D055FF", "#481567FF", "#2D708EFF"), 
             lwd = 3, kind = "sd", conf = 0.90, label = FALSE)
 
 legend("bottomright", legend = c("1", "2", "3"),
@@ -52,7 +58,7 @@ legend("bottomright", legend = c("1", "2", "3"),
 
 # PERMANOVA tests whether the group centroid of communities differs among groups
 # in multivariate space (e.g. different community composition)
-adonis2(dis.matrix ~ dat$trt, permutations = 999)
+adonis2(dis.matrix ~ dat$Trt, permutations = 999)
   #not significant
 
 ######################################################################
@@ -60,10 +66,14 @@ adonis2(dis.matrix ~ dat$trt, permutations = 999)
 #THIS IS WHERE I NEED HELP
 
 periods <- read_csv("C:/Users/mgj06/OneDrive/Documents/grad/decomp2023final.csv")
-periods$site <- as.factor(periods$Site)
-periods$trt <- as.factor(periods$Trt)
-periods$trap <- as.factor(periods$Trap)
-periods$datecollected <- as.factor(periods$DateCollected)
+periods <- read.csv("./decomp2023final.csv")
+str(periods)
+
+periods$Site <- as.factor(periods$Site) # I changed these so the capitization matched between variables
+periods$Trt <- as.factor(periods$Trt)
+periods$Trap <- as.factor(periods$Trap)
+periods$DateCollected <- as.factor(periods$DateCollected)
+str(periods)
 
 #add column for month
 table(periods$DateCollected)
@@ -76,9 +86,12 @@ periods$month <- as.factor(periods$month)
 
 periods$summer <- ifelse(periods$month=="8" | periods$month=="9", "late", "early")
 periods$summer <- as.factor(periods$summer) 
+str(periods)
 
-periods$trt_summer <- interaction(periods$trt, periods$summer)
+periods$trt_summer <- interaction(periods$Trt, periods$summer)
 table(periods$trt_summer)
+str(periods)
+
 #matrix
 dis.matrix <- vegdist(periods[,6:23], method = "bray", na.rm=TRUE)
 dis.matrix
@@ -93,13 +106,13 @@ stressplot(nmds.decom)
 plot(nmds.decom) # basic plot with no trt distinctions
 
 #plot
-ordiplot(nmds.decom, disp = "sites", type = "n", xlim = c(-2.5, 2.5), ylim = c(-2.5, 2.5))
-points(nmds.decom, dis = "sites", select = which(periods$trt=="1" & periods$summer=="early"), pch = 0, cex = 2, col = "#08f590")
-points(nmds.decom, dis = "sites", select = which(periods$trt=="1" & periods$summer=="late"), pch = 15, cex = 2, col = "#08f590")
-points(nmds.decom, dis = "sites", select = which(periods$trt=="2" & periods$summer=="early"), pch = 1, cex = 2, col = "#f5d608")
-points(nmds.decom, dis = "sites", select = which(periods$trt=="2" & periods$summer=="late"), pch = 16, cex = 2, col = "#f5d608")
-points(nmds.decom, dis = "sites", select = which(periods$trt=="3" & periods$summer=="early"), pch = 2, cex = 2, col = "#0898f5")
-points(nmds.decom, dis = "sites", select = which(periods$trt=="3" & periods$summer=="late"), pch = 17, cex = 2, col = "#0898f5")
+ordiplot(nmds.decom, disp = "sites", type = "n", xlim = c(-3.5, 2.5), ylim = c(-1.5, 1.0))
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="1" & periods$summer=="early"), pch = 0, cex = 2, col = "#08f590")
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="1" & periods$summer=="late"), pch = 15, cex = 2, col = "#08f590")
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="2" & periods$summer=="early"), pch = 1, cex = 2, col = "#f5d608")
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="2" & periods$summer=="late"), pch = 16, cex = 2, col = "#f5d608")
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="3" & periods$summer=="early"), pch = 2, cex = 2, col = "#0898f5")
+points(nmds.decom, dis = "sites", select = which(periods$Trt=="3" & periods$summer=="late"), pch = 17, cex = 2, col = "#0898f5")
 levels(periods$trt)
 levels(periods$summer)
 ordiellipse(nmds.decom, groups = periods$trt_summer, draw = "lines", col = c("#08f590", "#f5d608", "#0898f5"), 
@@ -108,9 +121,19 @@ ordiellipse(nmds.decom, groups = periods$trt_summer, draw = "lines", col = c("#0
 legend("bottomright", legend = c("1", "2", "3"),
        pch = c(17, 18, 15), cex = 1.5, bty = "n", col = c("#08f590", "#f5d608", "#0898f5"))
 
-adonis2(dis.matrix ~ periods$trt * periods$summer, permutations = 999)
-pairwise.adonis(periods$trt , periods$summer)
+adonis2(dis.matrix ~ periods$Trt * periods$summer, permutations = 999)
+pairwise.adonis(dis.matrix, periods$summer)
+pairwise.adonis(dis.matrix, periods$Trt)
+pairwise.adonis2(dis.matrix, periods$Trt * periods$summer)
+# I read through the github details and some additional information about pairwise.adonis in a forum
+# There is a pairwise.adonis2 in development that should be able to handle interaction terms, but it
+# doesn't appear to be ready yet, which is why you get the error
 #help with this pairwise not working
+
+adonis2(dis.matrix ~ periods$Trt * periods$month, permutations = 999)
+
+# you have a strong impact of time here, whether viewed as month or early/late
+# I am surprised that there isn't a time * treatment interaction, especially for 2 in yellow.
 ######################################################################
 
 
